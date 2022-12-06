6f9bd8fa7f80d2827e1db6359530aab26120b6d0
cr4isin <pries.ryan@gmail.com>
2022-12-05 20:28:04 -0600

    day 5

---
+++ day_5/puzzle_1.py
@@ -0,0 +1,19 @@
+
+from re import findall
+
+with open('day_5\\input.txt') as file:
+    # Read file
+    file_text = file.read().splitlines()
+    config = file_text[:8]
+    instructions = file_text[10:]
+    # Get stacks as strings
+    stacks = zip(*([line[4*i:4*(i+1)] for i in range(len(line)//4 + 1)] for line in config))
+    stacks_text = [''.join(reversed(findall('(\w)', ''.join(line)))) for line in stacks]
+    # Operate instructions on stacks
+    for line in instructions:
+        amount, from_col, to_col = map(int, findall('move (\d+) from (\d+) to (\d+)', line)[0])
+        for _ in range(amount):
+            stacks_text[to_col-1] += stacks_text[from_col-1][-1]
+            stacks_text[from_col-1] = stacks_text[from_col-1][:-1]
+
+    print(''.join(t[-1] for t in stacks_text))
