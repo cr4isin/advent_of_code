a9c3f69e008d4d2eb9a3131e83c6006f890a8efb
cr4isin <pries.ryan@gmail.com>
2022-12-07 15:46:19 -0600

    day 7

---
+++ day_7/puzzle_2.py
@@ -0,0 +1,28 @@
+
+from collections import defaultdict
+from re import findall
+
+# Determine the total size of the files in each directory
+position = 'C:'
+dir_size = defaultdict(int)
+with open('day_7\\input.txt') as file:
+    for line in file.read().splitlines():
+        # Move current directory
+        if matches := findall('\$ cd ([\w./]+)', line):
+            if matches[0] == '/':
+                position = 'C:'
+            elif matches[0] == '..':
+                position = '\\'.join(position.split('\\')[:-1])
+            else:
+                position += '\\' + matches[0]
+            dir_size[position] += 0
+        # Add file size if a file was found in current directory
+        elif matches := findall('(\d+) [\w.]+', line):
+            dir_size[position] += int(matches[0])
+
+# Calculate total dir sizes
+total_dir_size = dict()
+for key1 in dir_size:
+    total_dir_size[key1] = sum(dir_size[key2] for key2 in dir_size if key2.startswith(key1))
+
+print(min(x for x in total_dir_size.values() if x >= (30000000 - (70000000 - total_dir_size['C:']))))
