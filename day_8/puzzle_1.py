8d251bfa519ce7568469d20e717a0a0506b2f99d
cr4isin <pries.ryan@gmail.com>
2022-12-09 10:47:08 -0600

    day 8

---
+++ day_8/puzzle_1.py
@@ -0,0 +1,15 @@
+
+with open('day_8\\input.txt') as file:
+    tree_grid = [list(map(int, line)) for line in file.read().splitlines()]
+n = len(tree_grid)
+m = len(tree_grid[0])
+visible_trees = 2*n + 2*m - 4
+for i in range(1, n-1):
+    for j in range(1, m-1):
+        visible_trees += any([
+            all(tree_grid[k][j] < tree_grid[i][j] for k in range(0, i)),
+            all(tree_grid[k][j] < tree_grid[i][j] for k in range(i+1, n)),
+            all(tree_grid[i][k] < tree_grid[i][j] for k in range(0, j)),
+            all(tree_grid[i][k] < tree_grid[i][j] for k in range(j+1, m))
+        ])
+print(visible_trees)
---
+++ day_8/puzzle_2.py
@@ -0,0 +1,27 @@
+
+with open('day_8\\input.txt') as file:
+    tree_grid = [list(map(int, line)) for line in file.read().splitlines()]
+n = len(tree_grid)
+m = len(tree_grid[0])
+max_scenic_score = 0
+for i in range(1, n-1):
+    for j in range(1, m-1):
+        north = south = east = west = 0
+        for k in reversed(range(0,i)):
+            north += 1
+            if tree_grid[k][j] >= tree_grid[i][j]:
+                break
+        for k in range(i+1,n):
+            south += 1
+            if tree_grid[k][j] >= tree_grid[i][j]:
+                break
+        for k in reversed(range(0,j)):
+            west += 1
+            if tree_grid[i][k] >= tree_grid[i][j]:
+                break
+        for k in range(j+1,m):
+            east += 1
+            if tree_grid[i][k] >= tree_grid[i][j]:
+                break
+        max_scenic_score = max(max_scenic_score, north*east*south*west)
+print(max_scenic_score)
